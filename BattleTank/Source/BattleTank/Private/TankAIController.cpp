49b748b340ecf472a3e6153d654576309d5a6f8d
Peter Nguyen <spacechannel6@gmail.com>
2020-06-01 19:36:19 +0100

    added second tank to world map. added function for ai controller to find the player tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -8,12 +8,23 @@ ATank* ATankAIController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	return Cast<ATank>(PlayerPawn);
+
+}
+
+
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play"));
 
 	auto ControlledTank = GetControlledTank();
+
+	auto PlayerTank = GetPlayerTank();
 
 	if (!ControlledTank)
 	{
@@ -22,5 +33,14 @@ void ATankAIController::BeginPlay()
 	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AIController is possessing tank - %s"), *ControlledTank->GetName());
+	}
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player tank - %s"), *PlayerTank->GetName());
 	}
 }
