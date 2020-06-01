9d0e725ebad59bc6c7f637de48dbae40597c7d35
Peter Nguyen <spacechannel6@gmail.com>
2020-06-01 18:09:03 +0100

    added basic tank and tankplayer controller, reassigned player controller parent to posess tank at game start

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankIsNotPosessed"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankIsPosessed"));
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *ControlledTank->GetName());
+	}
+}
