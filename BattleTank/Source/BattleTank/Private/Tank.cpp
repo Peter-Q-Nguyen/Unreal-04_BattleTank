089ded983d4f7aa8471dface4ab03f7b40409ef2
Peter Nguyen <spacechannel6@gmail.com>
2020-06-08 23:20:36 +0100

    calculate elevation change and clamp it.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -38,6 +38,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+	if (TankAimingComponent != nullptr)
+		TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 
--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,6 +7,11 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//Move the barrel the right amount this frame
 	// Given a max elevation speed, and the time frame
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	
+	auto ClampedElevation = FMath::Clamp(RawNewElevation, MinElevationInDegrees, MaxElevationInDegrees);
 
 
+	SetRelativeRotation(FRotator(ClampedElevation, 0, 0));
 }
