a30244aee76b3a1d6534360ded0c5d11878dcdd4
Peter Nguyen <spacechannel6@gmail.com>
2020-06-17 03:26:09 +0100

    add enum to TankAimingComponent, expose to blueprint and set enum colours (by binding)  in playerUI_BP.

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,14 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked,
+};
+
 //forward declaration
 class UTankBarrel; 
 class UTankTurret;
@@ -25,9 +33,15 @@ public:
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
+
+
 
 	void MoveBarrelTowards(FVector AimDirection);
 	
